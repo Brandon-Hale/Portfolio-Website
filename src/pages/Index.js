5d1e3a62f543e0723470411a328f82a5c8ee1f83
Brandon Hale <brandon_hale@ozemail.com.au>
2025-05-25 15:56:45 +0800

    Some updates to page sections
    more personal side

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,10 +5,7 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={
-      "Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-      + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'
-    }
+    description="Brandon Hale's personal website. perth, Australia based Software Engineer."
   >
     <article className="post" id="index">
       <header>
@@ -17,23 +14,20 @@ const Index = () => (
             <Link to="/">About this site</Link>
           </h2>
           <p>
-            A beautiful, responsive, statically-generated, react application
-            written with modern Javascript.
+            This is my personal portfolio site — a space where I share my journey,
+            from education and work experience to projects and skills.
+            I’ve built it to be simple, easy to navigate,
+            and a reflection of who I am and what I enjoy doing in day to day life.
           </p>
         </div>
       </header>
       <p>
         {' '}
-        Welcome to my website. Please feel free to read more{' '}
+        Welcome to my portfolio. Please feel free to read more{' '}
         <Link to="/about">about me</Link>, or you can check out my{' '}
         <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
-        view <Link to="/stats">site statistics</Link>, or{' '}
+        view some <Link to="/stats">stats</Link> about me, or{' '}
         <Link to="/contact">contact</Link> me.
-      </p>
-      <p>
-        {' '}
-        Source available{' '}
-        <a href="https://github.com/mldangelo/personal-site">here</a>.
       </p>
     </article>
   </Main>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -26,7 +26,7 @@ const sections = {
 const Resume = () => (
   <Main
     title="Resume"
-    description="Michael D'Angelo's Resume. Smile Identity, Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet, and Facebook."
+    description="Brandon Hale's Resume. Software Engineer skilled in .NET Core, Typescript and SQL Databases."
   >
     <article className="post" id="resume">
       <header>
